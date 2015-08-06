c83f8fa3c7d466630bb39ec801ab4b7c37952fe1
santhubairamcs <santhubairamcs@gmail.com>
2015-08-06 11:51:18 +0530

    #0007 | taking items from console and tokenizing them

--- src/com/tax/Main.java
+++ src/com/tax/Main.java
@@ -1,4 +1,11 @@
 package com.tax;
 
 public class Main {
+
+    public static void main(String[] args) {
+        ComputeTax computeTax = new ComputeTax();
+        TaxRate taxRate = new TaxRate(10, 5);
+        ShoppingApp shoppingApp = new ShoppingApp(computeTax, taxRate);
+        shoppingApp.start();
+    }
 }
--- src/com/tax/ShoppingApp.java
+++ src/com/tax/ShoppingApp.java
@@ -1,4 +1,33 @@
 package com.tax;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ShoppingApp {
+
+    private final ComputeTax computeTax;
+    private final TaxRate taxRate;
+    ArrayList<String[]> items = new ArrayList<String[]>();
+
+    public ShoppingApp(ComputeTax computeTax, TaxRate taxRate) {
+        this.computeTax = computeTax;
+        this.taxRate = taxRate;
+    }
+
+    public void start() {
+        Scanner input = new Scanner(System.in);
+        String item;
+        do {
+            System.out.print("Enter an item or quit: ");
+            item = input.nextLine();
+            tokenize(item);
+        } while (!item.equals("quit"));
+    }
+
+    private void tokenize(String item) {
+        String[] tokensOfItem = item.split(" ");
+        items.add(tokensOfItem);
+    }
+
+
 }
